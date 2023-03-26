d2030dcaeb46016e0a3abfe4f471094de46214fc
Connor Broad <connor.broad@gmail.com>
2023-03-27 09:11:14 +1300

    City blocking in + input system

--- Assets/Levels/CityStreets/Scripts/PlayerCityToken.cs
+++ Assets/Levels/CityStreets/Scripts/PlayerCityToken.cs
@@ -1,10 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerCityToken : MonoBehaviour
 {
     private static readonly int PlayerPosShaderRef = Shader.PropertyToID("_PlayerPos");
+    
+    [SerializeField] private InputActionAsset actions;
+    private InputAction _moveAction;
+
+    private void Awake()
+    {
+        _moveAction = actions.FindActionMap("Player_Map").FindAction("Movement");
+        _moveAction.Enable();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +25,12 @@ public class PlayerCityToken : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        var moveVector = _moveAction.ReadValue<Vector2>();
+        print(moveVector);
+    }
+
+    void LateUpdate()
     {
         Shader.SetGlobalVector(PlayerPosShaderRef, transform.position);
     }
